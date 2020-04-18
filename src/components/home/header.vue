a9852e87cd3d57efc644053dba3cb0dcf64d2d2f
Vrijraj Singh <vrijraj2396@gmail.com>
2020-04-18 16:07:02 +0530

    First Commit

---
+++ src/components/home/header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        <h1>Home Header</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores a quam sint magnam modi fugiat obcaecati? Tenetur, laudantium cumque! Quos accusantium earum magnam quae possimus reprehenderit nostrum dolores, aliquid porro.</p>
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HomeComponent/>
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeComponent from '@/components/home/header'
+  export default {
+    name:'HomeComponent23',
+    components:{
+      HomeComponent
+    },
+    data:()=>({
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    }),
+    methods:{
+
+    }
   }
-}
 </script>
