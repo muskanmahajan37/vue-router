8862ac5a7943bd61c6a8c0a842bbd7ad3ec644ae
Vrijraj Singh <vrijraj2396@gmail.com>
2020-04-21 16:00:30 +0530

    New Code and Porps and Emit adedd

---
+++ src/components/home/card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{data.title}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{data.subtitle}}</h6>
+            <p class="card-text">{{data.des}}</p>
+            <a href="#" v-on:click="showData" class="card-link">Card link</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CardComponennt',
+        props:['data'],
+        data:()=>({
+
+        }),
+        methods:{
+            showData(){
+                this.$emit('SuccessFunction')
+            }
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,18 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
+        <p>Total ClICK = {{a}}</p>
         <HomeStartScreen/>
       </div>
     </div>
-
+    <div class="row py-4">
+      <div class="col-md-4">
+        <CardComponent @SuccessFunction="showAlert" :data="cardData"/>
+      </div>
+      <div class="col-md-4">
+        <CardComponent @SuccessFunction="showAlert" :data="cardData1"/>
+      </div>
+    </div>
     <div class="row py-4" style="background-color:#e0e0e0">
       <div class="col-md-12">
         <SkillsComponent/>
@@ -33,19 +41,34 @@ import HomeComponent from '@/components/home/header'
 import HomeStartScreen from '@/components/home/homestartscreen'
 import SkillsComponent from '@/components/home/skillset'
 import ProjectComponent from '@/components/home/projects'
+import CardComponent from '@/components/home/card'
   export default {
     name:'HomeComponent23',
     components:{
       HomeComponent,
       HomeStartScreen,
       SkillsComponent,
-      ProjectComponent
+      ProjectComponent,
+      CardComponent
     },
     data:()=>({
-
+      a:0,
+      cardData:{
+        title:'Tilte',
+        subtitle:'Subtitle',
+        des:'This is descriptionofthe cards'
+      },
+      cardData1:{
+        title:'Tilte1',
+        subtitle:'Subtitle1',
+        des:'This is descriptionofthe cards1'
+      },
     }),
     methods:{
-
+      showAlert(){
+        this.a =  this.a+1
+        alert('Kuch to calling')
+      }
     }
   }
 </script>
